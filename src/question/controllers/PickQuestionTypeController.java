cf5a66681db72fb21c07799af8355903068c6328
kendallgassner <kendall.gassner@yahoo.com>
2015-11-03 22:49:37 -0800

    Created views/controllers and models in questions

--- src/question/controllers/PickQuestionTypeController.java
+++ src/question/controllers/PickQuestionTypeController.java
@@ -1,30 +1,38 @@
 package question.controllers;
-
-import java.io.IOException;
-
 import classpack.controllers.ClassPackController;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.scene.control.*;
-import javafx.scene.control.Label;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 import test.controllers.TestController;
 import user.controllers.UserController;
 
-public class QuestionController {
+import java.io.IOException;
 
-    public Label testLabel;
+
+public class PickQuestionTypeController {
+
     public ChoiceBox selectSection = new ChoiceBox();
     public ChoiceBox selectQuestionType = new ChoiceBox();
-    public Button addQuestionButton = new Button();
-
+    public Button EnterQuestionTypeSelection = new Button();
+    public RadioButton FillInTheBlankButton = new RadioButton();
+    public RadioButton MultipleChoiceButton = new RadioButton();
+    public RadioButton MultipleSelectButton  = new RadioButton();
+    public RadioButton FreeResponseButton  = new RadioButton();
+    public RadioButton ShortAnswerButton  = new RadioButton();
+    public RadioButton CodingButton  = new RadioButton();
+    public RadioButton UMLButton = new RadioButton();
+    public RadioButton MatchingButton = new RadioButton();
 
     protected Stage currStage;
 
-    public void populateInterface(Stage stage){
+    public void populateInterface(Stage stage) {
         currStage = stage;
         selectSection.setItems(FXCollections.observableArrayList("Questions", "Tests", "Classes", "Settings"));
         selectSection.getSelectionModel().select(0);
@@ -32,7 +40,7 @@ public class QuestionController {
         selectQuestionType.setItems(FXCollections.observableArrayList("T/F", "Fill in the blank", "SA", "MC"));
     }
 
-    public void sectionChange(ActionEvent actionEvent) throws IOException{
+    public void sectionChange(ActionEvent actionEvent) throws IOException {
         if (selectSection.getValue() == "Tests") {
             FXMLLoader parentLoader = new FXMLLoader(getClass().getResource("../../test/views/AddTestView.fxml"));
             Parent nextSceneParent = parentLoader.load();
@@ -43,8 +51,7 @@ public class QuestionController {
 
             currStage.setScene(nextScene);
             currStage.show();
-        }
-        else if (selectSection.getValue() == "Classes") {
+        } else if (selectSection.getValue() == "Classes") {
             FXMLLoader parentLoader = new FXMLLoader(getClass().getResource("../../classpack/views/ClassView.fxml"));
             Parent nextSceneParent = parentLoader.load();
             Scene nextScene = new Scene(nextSceneParent);
@@ -54,8 +61,7 @@ public class QuestionController {
 
             currStage.setScene(nextScene);
             currStage.show();
-        }
-        else if (selectSection.getValue() == "Settings") {
+        } else if (selectSection.getValue() == "Settings") {
             FXMLLoader parentLoader = new FXMLLoader(getClass().getResource("../../user/views/SettingsView.fxml"));
             Parent nextSceneParent = parentLoader.load();
             Scene nextScene = new Scene(nextSceneParent);
@@ -72,18 +78,21 @@ public class QuestionController {
         System.out.println("Selected");
     }
 
-
-    public void PickNewQuestion() throws IOException {
-        FXMLLoader parentLoader = new FXMLLoader(getClass().getResource("../views/PickQuestionType.fxml"));
+    public void  QuestionTypeChosen() throws IOException {
+        FXMLLoader parentLoader = new FXMLLoader(getClass().getResource("../views/first.fxml"));
         Parent nextSceneParent = parentLoader.load();
         Scene nextScene = new Scene(nextSceneParent);
 
-        PickQuestionTypeController test = parentLoader.getController();
+        QuestionController test = parentLoader.getController();
         test.populateInterface(currStage);
 
         currStage.setScene(nextScene);
         currStage.show();
-        System.out.println("Select a Question type.");
+        System.out.println("Question type was selected.");
     }
+
+
+
+
 
 }
