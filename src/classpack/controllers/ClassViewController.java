b3fa7a5d3522cb54e284b73d580c0c17a8bac387
Alex Greene <alexgrn7@gmail.com>
2015-11-03 22:10:26 -0800

    going for broke

---
+++ src/classpack/controllers/ClassViewController.java
@@ -0,0 +1,19 @@
+package classpack.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+import question.controllers.QuestionController;
+import test.controllers.TestController;
+import user.controllers.UserController;
+
+import java.io.IOException;
+
+public class ClassViewController extends ClassPackController{
+
+
+}
