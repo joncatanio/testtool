eb6505c7ffb452c2c603e3772b526a35ea4f611d
Jonathan Catanio <JonCatanio@pcp135965pcs.wireless.calpoly.edu>
2015-11-05 22:11:41 -0800

    Updated Generate stuff, and added Models

---
+++ src/test/models/TestHandlerModel.java
@@ -0,0 +1,10 @@
+package test.models;
+
+/**
+ * Created by JonCatanio on 11/5/15.
+ */
+public class TestHandlerModel {
+    public void regrade(int id) {
+        System.out.println("Test " + id + " has been regraded.");
+    }
+}
