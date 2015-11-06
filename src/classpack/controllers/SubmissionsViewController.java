775a6f8474f6ae414535e53921e8721cd95c591c
Alex Greene <alexgrn7@gmail.com>
2015-11-05 20:45:07 -0800

    out no longer ignored; added class model

--- src/classpack/controllers/SubmissionsViewController.java
+++ src/classpack/controllers/SubmissionsViewController.java
@@ -4,12 +4,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 import java.io.IOException;
-
+import java.util.*;
+import classpack.models.*;
 
 public class SubmissionsViewController extends ClassTestsViewController{
 
+    public Label classAverageLabel;
+    public ListView submissionsList;
+    public Button regradeButton;
+
+    // list of student submission objects goes here
+    ArrayList<String> submissions = new ArrayList<String>();
+
+    public void populateSubmissionsList() {
+
+        ClassModel currClass = new ClassModel();
+        submissions = currClass.getSubmissionsFromBin(777);
+
+        for (int i = 0; i < submissions.size(); i++) {
+            submissionsList.getItems().add(new CheckBox(submissions.get(i)));
+        }
+
+    }
 
     public void viewAssignedTest(ActionEvent actionEvent) throws IOException {
 
@@ -22,5 +44,8 @@ public class SubmissionsViewController extends ClassTestsViewController{
 
         currStage.setScene(nextScene);
         currStage.show();
+    }
+
+    public void regradeSelectedSubmissions(ActionEvent actionEvent) {
     }
 }
